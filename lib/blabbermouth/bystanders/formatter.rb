ddea7f9d48acd44997e0cc3d007cdc58c9dcce01
Mark Rebec <mark@markrebec.com>
2015-05-07 19:39:50 -0700

    break out timestamps into a method for easier testing

--- lib/blabbermouth/bystanders/formatter.rb
+++ lib/blabbermouth/bystanders/formatter.rb
@@ -1,8 +1,12 @@
 module Blabbermouth
   module Bystanders
     module Formatter
+      def timestamp
+        ::Time.now.strftime('%Y/%m/%d %H:%M:%S %Z')
+      end
+
       def log_message(event, key, msg, data={})
-        message = "[#{::Time.now.strftime('%Y/%m/%d %H:%M:%S %Z')}] Blabbermouth.#{event.to_s}: #{key.to_s}"
+        message = "[#{timestamp}] Blabbermouth.#{event.to_s}: #{key.to_s}"
         message += ": #{msg.to_s}" unless msg.to_s.blank?
         message += " #{data.to_s}"
         message
