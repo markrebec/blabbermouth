2bd7d1d59e2e93b1a0154ebcff846fc15efcbc0a
Mark Rebec <mark@markrebec.com>
2015-05-07 20:40:14 -0700

    renames bystander blab method to relay

--- lib/blabbermouth/bystanders/dynamic_events.rb
+++ lib/blabbermouth/bystanders/dynamic_events.rb
@@ -3,8 +3,12 @@ module Blabbermouth
     module DynamicEvents
       EXCLUDES = [:flush]
 
+      def relay(meth, key, *args)
+        raise NotImplementedError, "#{self.class.name}##{__method__} has not been implemented"
+      end
+
       def method_missing(meth, *args)
-        blab meth, *args
+        relay meth, *args
       end
 
       def respond_to_missing?(meth, include_private=false)
--- lib/blabbermouth/bystanders/stdout.rb
+++ lib/blabbermouth/bystanders/stdout.rb
@@ -5,26 +5,26 @@ module Blabbermouth
       include Blabbermouth::Bystanders::DynamicEvents
 
       def error(key, e, *args)
-        blab :error, key, e, *args
+        relay :error, key, e, *args
       end
 
       def info(key, msg=nil, *args)
-        blab :info, key, msg, *args
+        relay :info, key, msg, *args
       end
 
       def increment(key, by=1, *args)
-        blab :increment, key, by, *args
+        relay :increment, key, by, *args
       end
 
       def count(key, total, *args)
-        blab :count, key, total, *args
+        relay :count, key, total, *args
       end
 
       def time(key, duration, *args)
-        blab :time, key, duration, *args
+        relay :time, key, duration, *args
       end
 
-      def blab(meth, key, *args)
+      def relay(meth, key, *args)
         data, opts, args = parse_args(*args)
         puts meth, key, args.first, data
       end
